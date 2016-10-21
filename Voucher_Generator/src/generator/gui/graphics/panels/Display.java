6aca2ca6f13e56578b3600cedea68f5f52af83d7
Christiandel Rabe <chris.rabe14@gmail.com>
2016-10-21 16:49:54 +1300

    created a super class for all the displays. updated voucher panel

--- Voucher_Generator/src/generator/gui/graphics/panels/Display.java
+++ Voucher_Generator/src/generator/gui/graphics/panels/Display.java
@@ -1,5 +1,13 @@
-
+/*	File: Display.java
+ * 	
+ * 	Date			Author				Changes
+ * 	21 Oct 16		Chris Rabe			changed this to an abstract class
+ */
 package generator.gui.graphics.panels;
+
+import javax.swing.JPanel;
+
+import generator.gui.graphics.VControl.View;
 
 /**
  * This interface provides a method which could be called by the dialog objects
@@ -8,9 +16,36 @@ package generator.gui.graphics.panels;
  * 
  * @author Chris
  */
-public interface Display {
+@SuppressWarnings("serial")
+public abstract class Display extends JPanel {
+	protected View parent;
+
+	public Display(View parent) {
+		this.parent = parent;
+		initialise();
+	}
+
 	/**
 	 * Updates the scrollpane on the display
 	 */
-	public void update();
+	public abstract void update();
+
+	/**
+	 * Adds action listeners to the components of the display
+	 */
+	public abstract void addActionListeners();
+
+	/**
+	 * Creates the control panel for all the buttons related to this display.
+	 */
+	public abstract JPanel createControlPanel();
+
+	/**
+	 * Creates the panel which displays the scroll panel containing the JList.
+	 */
+	public abstract JPanel createScrollPanel();
+
+	private void initialise() {
+		// TODO fill this in
+	}
 }
--- Voucher_Generator/src/generator/gui/graphics/panels/VoucherPanel.java
+++ Voucher_Generator/src/generator/gui/graphics/panels/VoucherPanel.java
@@ -9,15 +9,40 @@ import javax.swing.JPanel;
 
 import generator.gui.graphics.VControl.View;
 
+/**
+ * This panel contains a scroll list which displays all the code inside the
+ * generator. It also contains buttons for modification of Code objects inside
+ * the generator.
+ * 
+ * @author Chris
+ */
 @SuppressWarnings("serial")
-public class VoucherPanel extends JPanel implements Display {
-	private View parent;
+public class VoucherPanel extends Display {
 
 	public VoucherPanel(View parent) {
-		this.parent = parent;
+		super(parent);
 	}
 
 	@Override
 	public void update() {
+		// TODO update the scroll panel and revalidate
+	}
+
+	@Override
+	public void addActionListeners() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public JPanel createControlPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public JPanel createScrollPanel() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
