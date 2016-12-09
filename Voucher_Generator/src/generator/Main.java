f9ff9693302b7ff2bf9555083281509d496d54ff
Christiandel Rabe <chris.rabe14@gmail.com>
2016-12-10 01:31:19 +1300

    Only created the main class. Will do some program designing soon.

---
+++ Voucher_Generator/src/generator/Main.java
@@ -0,0 +1,14 @@
+package generator;
+
+/**
+ * Starts the application.
+ * 
+ * @author Chris
+ */
+public class Main {
+	public static final String VERSION = "0.5.0";
+
+	public static void main(String[] args) {
+
+	}
+}
