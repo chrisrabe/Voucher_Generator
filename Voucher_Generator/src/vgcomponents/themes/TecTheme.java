50b0750d7e31b3d20126259f40587dcf58e679fe
Christiandel Rabe <chris.rabe14@gmail.com>
2016-12-16 02:00:19 +1300

    implemented a new theme

---
+++ Voucher_Generator/src/vgcomponents/themes/TecTheme.java
@@ -0,0 +1,48 @@
+package vgcomponents.themes;
+
+import java.awt.Color;
+
+/**
+ * This represents a technology theme. It has a dark and light gray alternating
+ * colours, with a light blue selection highlight.
+ * 
+ * @author Chris
+ */
+public class TecTheme implements IVGTheme {
+
+	@Override
+	public Color getForegroundColour() {
+		return new Color(255, 255, 255);
+	}
+
+	@Override
+	public Color getMainColour1() {
+		return new Color(64, 64, 64);
+	}
+
+	@Override
+	public Color getMainColour2() {
+		return new Color(40, 40, 40);
+	}
+
+	@Override
+	public Color getSelectedBGColour1() {
+		return new Color(56, 85, 116);
+	}
+
+	@Override
+	public Color getSelectedBGColour2() {
+		return new Color(56, 85, 116);
+	}
+
+	@Override
+	public Color getSelectedFGColour() {
+		return new Color(191, 220, 251);
+	}
+
+	@Override
+	public Color getBorderColour() {
+		return new Color(253, 253, 253);
+	}
+
+}
