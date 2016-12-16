125da19e1f7b68e3b8aa7abfa00bcde054211dbc
Christiandel Rabe <chris.rabe14@gmail.com>
2016-12-16 18:03:31 +1300

    implemented display controllers for the IOController

--- Voucher_Generator/src/generator/control/display/IDisplayController.java
+++ Voucher_Generator/src/generator/control/display/IDisplayController.java
@@ -1,5 +1,18 @@
 package generator.control.display;
 
+import javax.swing.JPanel;
+
+/**
+ * This represents a controller for the displays inside a view.
+ * 
+ * @author Chris
+ */
 public interface IDisplayController {
 
+	/**
+	 * Retrieves the display from the controller
+	 * 
+	 * @return
+	 */
+	public JPanel getDisplay();
 }
--- Voucher_Generator/src/generator/control/page/IOController.java
+++ Voucher_Generator/src/generator/control/page/IOController.java
@@ -1,6 +1,14 @@
 package generator.control.page;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JPanel;
+
 import generator.control.ApplicationController;
+import generator.control.display.IDisplayController;
+import generator.view.display.io.load.Load;
+import generator.view.display.io.save.Save;
 import generator.view.page.PageView;
 import generator.view.page.io.IO;
 
@@ -12,9 +20,11 @@ import generator.view.page.io.IO;
  */
 public class IOController extends PageController {
 	private IO ioView;
+	private Map<String, IDisplayController> displayControllers;
 
 	public IOController(ApplicationController main) {
 		super(main);
+		displayControllers = createControllers();
 	}
 
 	@Override
@@ -49,4 +59,55 @@ public class IOController extends PageController {
 		return tmp;
 	}
 
+	private Map<String, IDisplayController> createControllers() {
+		Map<String, IDisplayController> tmp = new HashMap<String, IDisplayController>();
+		tmp.put("save", createSaveController());
+		tmp.put("load", createLoadController());
+		return tmp;
+	}
+
+	// Display Controllers
+
+	private IDisplayController createLoadController() {
+		return new IDisplayController() {
+			private Save saveDisplay;
+
+			@Override
+			public JPanel getDisplay() {
+				if (saveDisplay == null) {
+					saveDisplay = new Save();
+					saveDisplay.addXmlBtnListener(e -> {
+						// XML Save Event Handler
+					});
+					saveDisplay.addTxtBtnListener(e -> {
+						// TXT Save Event Handler
+					});
+					saveDisplay.addPngBtnListener(e -> {
+						// PNG Save Event Handler
+					});
+				}
+				return saveDisplay;
+			}
+		};
+	}
+
+	private IDisplayController createSaveController() {
+		return new IDisplayController() {
+			private Load loadDisplay;
+
+			@Override
+			public JPanel getDisplay() {
+				if (loadDisplay == null) {
+					loadDisplay = new Load();
+					loadDisplay.addXmlBtnListener(e -> {
+						// XML Load Event Handler
+					});
+					loadDisplay.addTxtBtnListener(e -> {
+						// TXT Load Event Handler
+					});
+				}
+				return loadDisplay;
+			}
+		};
+	}
 }
