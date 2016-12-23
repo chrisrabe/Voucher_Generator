197ef255472c50809d8011e4aa6f4565051a416b
Christiandel Rabe <chris.rabe14@gmail.com>
2016-12-23 15:50:31 +1300

    added methods that add listeners to the generate display

--- Voucher_Generator/src/generator/view/display/voucher/generate/Generate.java
+++ Voucher_Generator/src/generator/view/display/voucher/generate/Generate.java
@@ -1,5 +1,7 @@
 package generator.view.display.voucher.generate;
 
+import java.awt.event.ActionListener;
+
 /**
  * This class is responsible for creating methods that interact with the display
  * components.
@@ -11,5 +13,29 @@ public class Generate extends GenerateDisplay {
 
 	public Generate() {
 		this.initialiseComponents();
+	}
+
+	// Button listener
+
+	public void addGenerateBtnListener(ActionListener listener) {
+		generateBtn.addActionListener(listener);
+	}
+
+	// Field Methods
+
+	public String getCharsField() {
+		return charsField.getText();
+	}
+
+	public void setCharsField(String text) {
+		this.charsField.setText(text);
+	}
+
+	public String getSizeField() {
+		return sizeField.getText();
+	}
+
+	public void setSizeField(String text) {
+		sizeField.setText(text);
 	}
 }
