34b2f5fdf05d8efd731f6d54d895ed9ca7d2a544
Christiandel Rabe <chris.rabe14@gmail.com>
2016-12-23 16:20:24 +1300

    added display controllers to the voucher controller

--- Voucher_Generator/src/generator/control/page/VoucherController.java
+++ Voucher_Generator/src/generator/control/page/VoucherController.java
@@ -1,12 +1,20 @@
 package generator.control.page;
 
-import javax.swing.JOptionPane;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import generator.control.display.IDisplayController;
 import generator.control.manager.code.CodeManager;
 import generator.helper.converter.ValueConverter;
 import generator.helper.exception.EmptyCollectionException;
 import generator.helper.exception.InvalidInputException;
 import generator.helper.exception.TimeoutException;
+import generator.view.display.voucher.add.Add;
+import generator.view.display.voucher.edit.Edit;
+import generator.view.display.voucher.generate.Generate;
 import generator.view.page.PageView;
 import generator.view.page.voucher.Voucher;
 
@@ -14,9 +22,12 @@ public class VoucherController extends PageController {
 
 	private Voucher voucherView;
 	private CodeManager codeManager;
-	private String[] cache; // gets stored if the view is not created yet
+
+	private String[] cache;
+	private Map<String, IDisplayController> displayControllers;
 
 	public VoucherController(CodeManager codeManager) {
+		displayControllers = createControllers();
 		this.codeManager = codeManager;
 	}
 
@@ -40,7 +51,7 @@ public class VoucherController extends PageController {
 		}
 	}
 
-	// Methods
+	// PageController Methods
 
 	/**
 	 * Changes the data inside the JList inside the voucher view. If the view
@@ -122,4 +133,69 @@ public class VoucherController extends PageController {
 		}
 		return line.split(" ")[0];
 	}
+
+	private Map<String, IDisplayController> createControllers() {
+		Map<String, IDisplayController> tmp = new HashMap<String, IDisplayController>();
+		tmp.put("add", createAddController());
+		tmp.put("edit", createEditController());
+		tmp.put("gen", createGenerateController());
+		return tmp;
+	}
+
+	// Display controllers
+
+	private IDisplayController createGenerateController() {
+		return new IDisplayController() {
+			private Generate generateDisplay;
+
+			@Override
+			public JPanel getDisplay() {
+				if (generateDisplay == null) {
+					generateDisplay = new Generate();
+					generateDisplay.addGenerateBtnListener(e -> {
+						// Add generate logic here
+					});
+				}
+				return generateDisplay;
+			}
+		};
+	}
+
+	private IDisplayController createEditController() {
+		return new IDisplayController() {
+			private Edit editDisplay;
+
+			@Override
+			public JPanel getDisplay() {
+				if (editDisplay == null) {
+					editDisplay = new Edit();
+					editDisplay.addConfirmBtnListener(e -> {
+						// Edit logic here
+					});
+				}
+				return editDisplay;
+			}
+		};
+	}
+
+	private IDisplayController createAddController() {
+		return new IDisplayController() {
+			private Add addDisplay;
+
+			@Override
+			public JPanel getDisplay() {
+				if (addDisplay == null) {
+					addDisplay = new Add();
+					addDisplay.addGenerateBtnListener(e -> {
+						// generate code id here
+					});
+					addDisplay.addConfirmBtnListener(e -> {
+						// add logic here
+					});
+				}
+				return addDisplay;
+			}
+		};
+	}
+
 }
